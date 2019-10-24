392f5e788522599ff77069f786becb52e7cab0c0
doncity-tech <princewill.opara@outlook.com>
2019-10-24 22:12:34 +0100

    Yes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,9 +9,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number){
+  getAlbum(id: number) {
     return this._http.get(this._albumUrl).map((response) =>  response.json());
-    }
   }
 
 }
